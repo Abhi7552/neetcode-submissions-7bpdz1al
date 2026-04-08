c2871426927ac9302b18673145d97a6d636f2bfe
Abhishek Kumar <123100483+Abhi7552@users.noreply.github.com>
2026-04-08 22:08:40 +0530

    Add: excel-sheet-column-title - submission-1

---
+++ Data Structures & Algorithms/excel-sheet-column-title/submission-1.js
@@ -0,0 +1,19 @@
+class Solution {
+    /**
+     * @param {number} columnNumber
+     * @return {string}
+     */
+    convertToTitle(columnNumber) {
+        let ans="";
+        while(columnNumber>0){
+            columnNumber--;
+            let rem=columnNumber%26;
+
+            ans=String.fromCharCode(65+rem)+ans;
+            
+            columnNumber=Math.floor(columnNumber/26);
+        }
+
+        return ans;
+    }
+}
