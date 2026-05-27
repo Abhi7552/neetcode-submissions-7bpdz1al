76dd3ff8d0ff054776309237f30e287d7be4b52f
Abhishek Kumar <123100483+Abhi7552@users.noreply.github.com>
2026-05-27 20:05:32 +0530

    Add: simplify-path - submission-0

---
+++ Data Structures & Algorithms/simplify-path/submission-0.js
@@ -0,0 +1,22 @@
+class Solution {
+    /**
+     * @param {string} path
+     * @return {string}
+     */
+    simplifyPath(path) {
+        let stack = [];
+        let parts = path.split('/');
+
+        for (let part of parts) {
+            if (part === '' || part === '.') {
+                continue;
+            } else if (part === '..') {
+                if (stack.length > 0) stack.pop();
+            } else {
+                stack.push(part);
+            }
+        }
+
+        return '/' + stack.join('/');
+    }
+}
