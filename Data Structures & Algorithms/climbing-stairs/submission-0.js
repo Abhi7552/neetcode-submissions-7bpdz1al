d3d2287ca3529921a146f1df73a5c861b6035859
Abhishek Kumar <123100483+Abhi7552@users.noreply.github.com>
2026-07-07 16:55:25 +0530

    Add: climbing-stairs - submission-0

---
+++ Data Structures & Algorithms/climbing-stairs/submission-0.js
@@ -0,0 +1,20 @@
+class Solution {
+    /**
+     * @param {number} n
+     * @return {number}
+     */
+    climbStairs(n) {
+        let dp=new Array(n).fill(0);
+
+        dp[0]=0;
+        dp[1]=1;
+        dp[2]=2;
+        dp[3]=3;
+
+        for(let i=4;i<=n;i++){
+            dp[i]=dp[i-1]+dp[i-2];
+        }
+
+        return dp[n];
+    }
+}
